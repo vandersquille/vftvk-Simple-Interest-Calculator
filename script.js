8ba29c25408febe06d76fb6ba006d4c07b759b8e
Betsy <betsyantony02@gmail.com>
2021-06-16 11:28:01 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+
+// to update the range as per scrolling 
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+//to compute an interest only if amount is positive , if not then provides an alert
 function compute()
 {
-    p = document.getElementById("principal").value;
+   
+   var principal = document.getElementById("principal").value;
+
+var rate = document.getElementById("rate").value;
+
+var years = document.getElementById("years").value;
+
+var amount = principal * years * rate /100;
+
+var year = new Date().getFullYear()+parseInt(years);
+ 
     
+
+ p = document.getElementById("principal").value;
+
+    if(p < 1){
+                alert("Enter a positive number");
+                p.focus();
+                return false;
+          }
+    else{
+ document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    }      
+
 }
-        
+      
+//checks whether the amount is a positive number,  if not provides an alert
